1eff47d370a93197e559647b1672b1d643cee400
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2022-06-30 23:06:20 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Post } from "./Post";
+import { useEffect, useState } from "react";
+import {PaginationButtonsList} from './PaginationButtonsList';
 
+const PostList = (props) => {
+  const [postData, setPostData] = useState(null);
 
-const PostList = () => {
-    
-    return (
-        <>
+  const pageNo = props.pageNo;
+  const setPageNo = props.setPageNo;
 
-        </>
-    )
-}
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_page=${pageNo}&_limit=5`
+      );
+      const jsonRes = await response.json();
+      console.log(jsonRes);
+      setPostData(jsonRes);
+    };
+    fetchPosts();
+  }, [pageNo]);
 
-export { PostList }
+  return (
+    <>
+    {
+        postData==null?(
+            <div id="loader" className="loader">
+                loading
+            </div>
+        ):(
+            postData.map((post)=>{
+                return <Post
+                id={post.id}
+                key={post.id}
+                title={post.title}
+                body={post.body}
+              />
+            })
+        )
+    }
+    <PaginationButtonsList pageNo={pageNo} setPageNo={setPageNo}/>
+    </>
+  );
+};
+
+export { PostList };
